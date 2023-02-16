d27a26bd4fda1693d153bdcd1fd522366ff405f3
joelfriesecke <mail@joelfriesecke.de>
2023-02-16 16:50:15 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -14,5 +14,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), robotsTxt(), sitemap(), tailwind()]
+  integrations: [image(), robotsTxt(), sitemap(), tailwind()],
+  site: "https://joelfriesecke.de/",
 });
